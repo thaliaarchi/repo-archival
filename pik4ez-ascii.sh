67f74ede91f329a57f4274d122d3e95d2451d783
Andrew Archibald <andrew@aarchibald.com>
2021-10-29 17:39:59 +0200

    Redo pik4ez-ascii by cleaning brooks history

--- pik4ez-ascii.sh
+++ pik4ez-ascii.sh
@@ -1,74 +1,57 @@
 #!/bin/sh -e
 
-# https://gist.github.com/search?q=user%3Apik4ez+extension%3Aws
+# https://gist.github.com/pik4ez/a2b0ece78c999e3b9ca54369ffd484f3 brooks
+# https://gist.github.com/pik4ez/61c0678799dbd4a7fc5a6ca08959b7b4 hopper
+# https://gist.github.com/pik4ez/8274216220511d0e42de7881eca782da rameev
+# https://gist.github.com/pik4ez/fb76073886531d9b888a5c86e870bccd shura-bura
+# https://gist.github.com/pik4ez/e3d776091bae3a41e18ae1404bdccac1 lamarr
 
-git clone https://gist.github.com/pik4ez/a2b0ece78c999e3b9ca54369ffd484f3 brooks
-git clone https://gist.github.com/pik4ez/61c0678799dbd4a7fc5a6ca08959b7b4 hopper
-git clone https://gist.github.com/pik4ez/8274216220511d0e42de7881eca782da rameev
-git clone https://gist.github.com/pik4ez/fb76073886531d9b888a5c86e870bccd shura-bura
-git clone https://gist.github.com/pik4ez/e3d776091bae3a41e18ae1404bdccac1 lamarr
+# brooks contains the entirety of the other repos at earlier dates,
+# however the history is messy:
 
-git -C brooks filter-repo --path brooks.ws --commit-callback 'commit.message = b"Add brooks.ws" if len(commit.parents) == 0 else b"Update brooks.ws"'
-git -C hopper filter-repo --commit-callback 'commit.message = b"Add hopper.ws" if len(commit.parents) == 0 else b"Update hopper.ws"'
-git -C rameev filter-repo --commit-callback 'commit.message = b"Add rameev.ws" if len(commit.parents) == 0 else b"Update rameev.ws"'
-git -C shura-bura filter-repo --commit-callback 'commit.message = b"Add shura-bura.ws" if len(commit.parents) == 0 else b"Update shura-bura.ws"'
-git -C lamarr filter-repo --commit-callback 'commit.message = b"Add lamarr.ws" if len(commit.parents) == 0 else b"Update lamarr.ws"'
+# 1. add gistfile1.txt
+# 2. edit gistfile1.txt (remove placeholder header)
+# 3. rename gistfile1.txt -> hedi_ascii.ws
+# 4. add brooks.ws
+#    add hopper.ws
+#    rename hedi_ascii.ws -> lamarr.ws
+#    add rameev.ws
+#    add shura-bura.ws
+# 5. edit brooks.ws (replace placeholder)
+#    edit hopper.ws (replace placeholder)
+#    edit rameev.ws (replace placeholder)
+#    edit shura-bura.ws (replace placeholder)
+# 6. delete hopper.ws
+#    delete lamarr.ws
+#    delete rameev.ws
+#    delete shura-bura.ws
 
-git -C brooks branch -m master main
-git -C hopper branch -m master main
-git -C rameev branch -m master main
-git -C shura-bura branch -m master main
-git -C lamarr branch -m master main
+git clone https://gist.github.com/pik4ez/a2b0ece78c999e3b9ca54369ffd484f3 pik4ez
+cd pik4ez
+
+# Remove file deletions and fix lamarr.ws filename:
+git filter-repo \
+  --path-rename gistfile1.txt:lamarr.ws \
+  --path-rename hedi_ascii.ws:lamarr.ws \
+  --commit-callback '
+    commit.file_changes = [c for c in commit.file_changes if c.type != b"D"]
+    commit.message = commit.author_date'
 
 # Squash placeholder text commits:
-
-# pick b11d3e4 Add brooks.ws
-# squash 80e8765 Update brooks.ws
-git -C brooks rebase -i --root --committer-date-is-author-date
-# pick 9967a1a Add hopper.ws
-# squash b4f044a Update hopper.ws
-git -C hopper rebase -i --root --committer-date-is-author-date
-# pick 389083a Add rameev.ws
-# squash c32541c Update rameev.ws
-git -C rameev rebase -i --root --committer-date-is-author-date
-# pick f000050 Add shura-bura.ws
-# squash b15338c Update shura-bura.ws
-git -C shura-bura rebase -i --root --committer-date-is-author-date
-# pick 395f248 Add lamarr.ws
-# squash cf6687d Update lamarr.ws
-git -C lamarr rebase -i --root --committer-date-is-author-date
-
-git -C brooks filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
-git -C hopper filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
-git -C rameev filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
-git -C shura-bura filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
-git -C lamarr filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
-
-mkdir pik4ez
-cd pik4ez
-git init
-git remote add brooks ../brooks
-git remote add hopper ../hopper
-git remote add rameev ../rameev
-git remote add shura-bura ../shura-bura
-git remote add lamarr ../lamarr
-git fetch brooks
-git fetch hopper
-git fetch rameev
-git fetch shura-bura
-git fetch lamarr
-git merge --allow-unrelated-histories --no-edit brooks/main
-git merge --allow-unrelated-histories --no-edit hopper/main
-git merge --allow-unrelated-histories --no-edit rameev/main
-git merge --allow-unrelated-histories --no-edit shura-bura/main
-git merge --allow-unrelated-histories --no-edit lamarr/main
-git remote remove brooks
-git remote remove hopper
-git remote remove rameev
-git remote remove shura-bura
-git remote remove lamarr
-
-# Automatically sorts by author date and drops merge commits.
-# Accept change list as-is.
+#   pick 99e0669 1534347757 +0300
+#   squash 2e75f1c 1534354829 +0300
+#   pick 23b06bb 1534438966 +0300
+#   squash 138d65b 1534439105 +0300
 git rebase -i --root --committer-date-is-author-date
-git filter-repo -f --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
+
+# Set messages to names of files, since only changes are now adds:
+git filter-repo --commit-callback '
+  filenames = [c.filename for c in commit.file_changes]
+  if len(filenames) > 1:
+    filenames[-1] = b"and " + filenames[-1]
+  commit.message = b"Add " + b", ".join(filenames)
+  commit.committer_name = commit.author_name
+  commit.committer_email = commit.author_email'
+
+git branch -m master main
+git remote add origin https://github.com/wspace/pik4ez-ascii
