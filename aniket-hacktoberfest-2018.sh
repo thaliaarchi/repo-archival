373c10aea428380be088897e4c0c5390091bc148
Andrew Archibald <andrew@aarchibald.com>
2021-11-01 12:55:12 +0100

    Ignore changes that delete files in aniket-hacktoberfest-2018

--- aniket-hacktoberfest-2018.sh
+++ aniket-hacktoberfest-2018.sh
@@ -3,6 +3,11 @@
 git clone https://github.com/Aniket965/Hello-world aniket-hacktoberfest-2018
 cd aniket-hacktoberfest-2018
 # Ignore dirty tree from conflicting case-sensitive files with --force
-git filter-repo --force --subdirectory-filter Whitespace --path LICENSE
+git filter-repo --force \
+  --subdirectory-filter Whitespace --path LICENSE \
+  --commit-callback 'commit.file_changes = [c for c in commit.file_changes if c.type != b"D"]' \
+  --prune-degenerate=always
+git rebase --root --committer-date-is-author-date
+git filter-repo --commit-callback 'commit.committer_name = commit.author_name; commit.committer_email = commit.author_email'
 git branch -m master main
 git remote add origin https://github.com/wspace/aniket-hacktoberfest-2018
