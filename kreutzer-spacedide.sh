9357c2d609a5d81230500a58b1728d5c25db3d24
Andrew Archibald <andrew@aarchibald.com>
2022-06-30 02:18:25 -0600

    Extract programs from stephenchappell-python

--- kreutzer-spacedide.sh
+++ kreutzer-spacedide.sh
@@ -4,7 +4,7 @@ git svn clone --stdlayout https://svn.code.sf.net/p/spacedide/code spacedide
 git -C spacedide filter-repo --force \
   --message-callback '
     return b"Initial commit\n" if message.startswith(b"Initial commit") else re.sub(
-      rb"^\n+git-svn-id: https://svn.code.sf.net/p/spacedide/code@(\d+) 253eaa7e-a5eb-43a2-a586-e43a4db1c6d0\n$", rb"r\1\n", message)' \
+      br"^\n+git-svn-id: https://svn.code.sf.net/p/spacedide/code@(\d+) 253eaa7e-a5eb-43a2-a586-e43a4db1c6d0\n$", br"r\1\n", message)' \
   --commit-callback '
     commit.author_email = commit.author_name + b"@users.sourceforge.net"
     commit.committer_email = commit.committer_name + b"@users.sourceforge.net"'
---
+++ programs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+# TODO extract programs from more repos
+
+git clone https://github.com/wspace/stephenchappell-python
+git -C stephenchappell-python filter-repo \
+  --path LICENSE.md \
+  --path Assembly --path-rename Assembly/: \
+  --blob-callback 'if b"bmp.py" in blob.data: blob.skip()' \
+  --commit-callback '
+    if len(commit.file_changes) == 1 and commit.file_changes[0].filename == b"LICENSE.md":
+      commit.message = b"Update LICENSE.md\n"
+    else:
+      commit.message = re.sub(br"https://code\.activestate\.com\S+", b"https://github.com/wspace/stephenchappell-python/commit/" + commit.original_id, commit.message)
+  '
+# Manual rebase to squash LICENSE.md update commits
