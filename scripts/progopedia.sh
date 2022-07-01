e543891195a577c748690eb70422d8ed18de7e4f
Andrew Archibald <andrew@aarchibald.com>
2022-07-01 11:24:46 -0600

    Note more progopedia metadata

--- scripts/progopedia.sh
+++ scripts/progopedia.sh
@@ -19,6 +19,11 @@ get_article() {
       -o "$filename"
 }
 
+get_revisions() {
+  local url="$1history/"
+  curl --no-progress-meter "$url" | htmlq '#maintext > table' | html2md
+}
+
 mkdir progopedia
 cd progopedia
 git init -q
@@ -78,3 +83,31 @@ commit_nickolas '2011-07-27 15:33:00 +0000' '2011-07-27 15:33:00 +0000' \
 https://web.archive.org/web/20110827074120/http://progopedia.com:80/language/whitespace/'
 
 git remote add origin https://github.com/wspace/progopedia
+
+# http://progopedia.com/implementation/whitespacers/
+# 2010-09-14 18:53 Nickolas "New article."
+
+# http://progopedia.com/version/whitespacers-ruby/
+# 2010-09-14 18:56 Nickolas "New article."
+
+# http://progopedia.com/example/hello-world/208/
+# 2010-09-14 22:06 Nickolas "Changed text."
+# 2010-09-14 11:45 Nickolas "New article."
+
+# http://progopedia.com/example/factorial/222/
+# http://progopedia.com/example/fibonacci/223/
+# http://progopedia.com/example/factorial/222/
+# http://progopedia.com/example/camelcase/352/
+
+# http://progopedia.ru/language/whitespace/
+# 2010-12-03 20:14 Nickolas "Новая статья."
+# 2011-06-06 19:37 Nickolas "Изменен paradigms."
+# 2011-10-03 23:59 kit      "Изменен text."
+
+# http://progopedia.ru/implementation/esco-whitespace/
+# http://progopedia.ru/implementation/whitespacers/
+# http://progopedia.ru/version/whitespacers-ruby/
+# http://progopedia.ru/example/hello-world/163/
+# http://progopedia.ru/example/factorial/164/
+# http://progopedia.ru/example/fibonacci/165/
+# http://progopedia.ru/example/camelcase/390/
