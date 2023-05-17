de63ef898a74fb451c7882c3f225c661a3dbc95a
Thalia Archibald <thalia@archibald.dev>
2023-05-17 05:58:02 -0600

    cybis: Use CyBF server repo

--- scripts/cybis/cybf.sh
+++ scripts/cybis/cybf.sh
@@ -11,48 +11,41 @@ commit() {
 mkdir -p cybis
 cd cybis
 
-unzip -q ../../files/cybis/checkouts/CyBF.zip
+# SVN repo
+unzip -q ../../files/cybis/repos/CyBF.zip
+chmod -R u+rwX,go+rX,go-w CyBF
 mv CyBF cybf-svn
-cd cybf-svn
 
-# Repo metadata:
-#
-# $ svn info
-# URL: file:///C:/svn/CyBF
-# Relative URL: ^/
-# Repository Root: file:///C:/svn/CyBF
-# Repository UUID: c9d10fbd-33ba-bc4d-a72f-f4d6a9f61016
-# Revision: 60
-# Node Kind: directory
-# Schedule: normal
-# Last Changed Author: Kevin
-# Last Changed Rev: 60
-# Last Changed Date: 2017-05-19 08:28:52 +0000 (Fri, 19 May 2017)
+# SVN checkout
+unzip -q ../../files/cybis/checkouts/CyBF.zip
+mv CyBF cybf-svn-checkout
+svn relocate "file://$(pwd)/cybf-svn" cybf-svn-checkout
 
-# Create a clean directory tree
-svn export -q . ../cybf
-cd ../cybf
-git init -q
-git config core.autocrlf false
+# git conversion
+# It has trunk (which was renamed to CyBF), CyBF, and CyBF Library root
+# directories, which are used more like subprojects than branches and multiple
+# are sometimes updated in the same commit, so I don't use --stdlayout.
+git svn clone "file://$(pwd)/cybf-svn" cybf \
+  --authors-file=<(echo 'Kevin = Kevin <cybis-fdp@hotmail.com>')
+cd cybf
 
 # Convert svn:ignore to .gitignore
-#
-# $ svn propget svn:ignore -R
-#
-# CyBF - .vs
-# Notes
-# bin
-# obj
-#
-cp ../../../files/cybis/cybf/CyBF/.gitignore CyBF/
+# TODO convert .gitignore for all revisions
+git svn create-ignore
+GIT_COMMITTER_NAME="$(git show -s --format=%an)" \
+GIT_COMMITTER_EMAIL="$(git show -s --format=%ae)" \
+GIT_COMMITTER_DATE="$(git show -s --format=%ai)" \
+git commit --amend --no-edit
 
-# SVN working copies have no history, so commit the latest revision
-git add -Af
-commit '2017-05-19 08:28:52 +0000' 'Revision 60
+# Reformat git-svn metadata and collapse trunk
+git filter-repo -f --quiet \
+  --path-rename trunk/: \
+  --message-callback '
+    message = re.sub(br"^([\s\S]*?)\n*git-svn-id: .*@(\d+) c9d10fbd-33ba-bc4d-a72f-f4d6a9f61016\n$", br"[r\2] \1\n", message)
+    message = re.sub(br"^(\[r\d+\])\s+$", br"\1\n", message)
+    return message'
 
-https://github.com/thaliaarchi/repo-archival/blob/main/files/cybis/checkouts/CyBF.zip'
-
-# Untracked and ignored files:
+# Untracked and ignored files in SVN checkout:
 # zip modified time                Stat Filename
 # 2020-12-03 04:59:41.6404838 +0000  I  CyBF/.vs/*
 # 2017-05-24 19:21:23.8607907 +0000  I  CyBF/Notes/API Notes.txt
@@ -73,11 +66,20 @@ https://github.com/thaliaarchi/repo-archival/blob/main/files/cybis/checkouts/CyB
 # 2017-05-26 20:30:53.7985542 +0000  ?  CyBF Library/bfc.py
 # 2017-05-28 01:34:10.2815950 +0000  ?  CyBF Library/output.txt
 
-cp -Rp ../cybf-svn/CyBF/Notes CyBF/
+git config user.name Kevin
+git config user.email cybis-fdp@hotmail.com
+
+cp -Rp ../cybf-svn-checkout/CyBF/Notes CyBF/
+
+rebase_break_before_date '2017-05-24 19:21:23 +0000'
 git add -f 'CyBF/Notes/API Notes.txt'
 commit '2017-05-24 19:21:23 +0000' 'Add API notes'
+git rebase --continue
+
+rebase_break_before_date '2017-05-26 16:24:04 +0000'
 git add -f 'CyBF/Notes/Sudoku Notes.txt'
 commit '2017-05-26 16:24:04 +0000' 'Add sudoku notes'
+git rebase --continue
 
 # Sudoku.bf
 # zip last modified   2017-05-26 19:06:04.3278666 +0000                  CyBF Library/Sudoku.bf
@@ -85,16 +87,18 @@ commit '2017-05-26 16:24:04 +0000' 'Add sudoku notes'
 # zip last modified   2017-05-26 19:16:08.8186862 +0000                  CyBF Library/Sudoku.7z
 # HTTP Last-Modified  2017-05-26 20:14:55 +0000                          https://what.thedailywtf.com/assets/uploads/files/1495829695493-sudoku.7z
 # Posted              2017-05-26 20:34:53.111 +0000        Huf_Lungdung  https://what.thedailywtf.com/topic/23006/stupid-coding-tricks-sudoku-solver-in-brainfuck
-cmp '../cybf-svn/CyBF Library/Sudoku.7z' "$(get_cached_path https://what.thedailywtf.com/assets/uploads/files/1495829695493-sudoku.7z)"
-7z x '../cybf-svn/CyBF Library/Sudoku.7z'
-cmp '../cybf-svn/CyBF Library/Sudoku.bf' Sudoku.bf
+cmp '../cybf-svn-checkout/CyBF Library/Sudoku.7z' "$(get_cached_path https://what.thedailywtf.com/assets/uploads/files/1495829695493-sudoku.7z)"
+7z x '../cybf-svn-checkout/CyBF Library/Sudoku.7z'
+cmp '../cybf-svn-checkout/CyBF Library/Sudoku.bf' Sudoku.bf
 mv Sudoku.bf 'CyBF Library/'
+rebase_break_before_date '2017-05-26 19:06:04 +0000'
 git add 'CyBF Library/Sudoku.bf'
 commit '2017-05-26 19:06:04 +0000' 'Compile sudoku solver to Brainfuck
 
 https://what.thedailywtf.com/topic/23006/stupid-coding-tricks-sudoku-solver-in-brainfuck'
+git rebase --continue
 
-cp -p '../cybf-svn/CyBF Library/Sudoku.py' 'CyBF Library'
+cp -p '../cybf-svn-checkout/CyBF Library/Sudoku.py' 'CyBF Library'
 git add 'CyBF Library/Sudoku.py'
 commit '2017-05-26 20:31:50 +0000' 'Compile sudoku solver to Python'
 
@@ -102,6 +106,7 @@ git add -f 'CyBF/Notes/Recursion.txt'
 commit '2019-06-28 14:09:37 +0000' 'Add recursion notes'
 # Model Notes.txt is empty
 
-rm -rf ../cybf-svn
+git config --unset user.name
+git config --unset user.email
 
 git remote add origin https://github.com/thaliaarchi/cybf-archive
