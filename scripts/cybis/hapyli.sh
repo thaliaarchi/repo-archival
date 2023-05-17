0703a8bc42c83f2001f6f9480f5462bb10a7778d
Thalia Archibald <thalia@archibald.dev>
2023-05-17 15:53:30 -0600

    cybis: Reconstruct initial site version; group files by date

--- scripts/cybis/hapyli.sh
+++ scripts/cybis/hapyli.sh
@@ -63,9 +63,6 @@ git rebase --continue
 git config --unset user.name
 git config --unset user.email
 
-# Public release announcement:
-# 2010-05-23 02:20:21 +0000 Cybis FDP <cybis-fdp@hotmail.com> https://web.archive.org/web/20130926170259/http://compsoc.dur.ac.uk:80/archives/whitespace/2010-May/000075.html
-
 # Untracked and ignored files in SVN checkout:
 # zip modified time        Stat Filename
 # 2010-12-18 22:26:06 +0000  ?  99bottles.hpl
@@ -75,29 +72,12 @@ git config --unset user.email
 # 2009-05-10 07:44:54 +0000  I  hwc/*.pyc
 # 2009-04-28 05:21:52 +0000  I  wspace.exe
 
-cp -p ../hapyli-svn-checkout/99bottles.{hpl,ws} .
-git add 99bottles.{hpl,ws}
-GIT_AUTHOR_NAME='Marinus Oosters' GIT_AUTHOR_EMAIL='marinuso@gmail.com' GIT_AUTHOR_DATE='2010-11-27 00:00:00 +0000' \
-GIT_COMMITTER_NAME='Kevin' GIT_COMMITTER_EMAIL='cybis-fdp@hotmail.com' GIT_COMMITTER_DATE='2010-12-18 22:27:46 +0000' \
-git commit -q -m 'Add 99 bottles of beer in HaPyLi
-
-https://www.99-bottles-of-beer.net/language-hapyli-2556.html'
-
-# 2010-11-27 Marinus Oosters https://www.99-bottles-of-beer.net/language-hapyli-2556.html
-diff 99bottles.hpl "$(get_cached_path https://www.99-bottles-of-beer.net/download/2556)"
-
-# 2010-12-01 07:54 Marinus https://esolangs.org/w/index.php?title=User:Marinus/Brainfuck_interpreters&diff=20310&oldid=18866
-# Marinus appears to live in CET +0100 / CEST +0200 based on commits from https://github.com/marinuso
-cp ../../../files/cybis/hapyli/brainfuck.hpl .
-git add brainfuck.hpl # manually extracted from wiki
-GIT_AUTHOR_NAME='Marinus Oosters' GIT_AUTHOR_EMAIL='marinuso@gmail.com' GIT_AUTHOR_DATE='2010-12-01 08:54:00 +0100' \
-GIT_COMMITTER_NAME='Marinus Oosters' GIT_COMMITTER_EMAIL='marinuso@gmail.com' GIT_COMMITTER_DATE='2010-12-01 08:54:00 +0100' \
-git commit -q -m 'Add Brainfuck interpreter in HaPyLi
-
-https://esolangs.org/wiki/User:Marinus/Brainfuck_interpreters#HaPyLi'
-
 # Apply the differences from the Webs site to the local tutorial, as if it was
-# not mangled by the site builder tool.
+# not mangled by the site builder tool. Additionally, remove 99bottles.hpl from
+# Examples, although that snapshot was not saved.
+#
+# Public release announcement:
+# 2010-05-23 02:20:21 +0000  Cybis FDP <cybis-fdp@hotmail.com>  https://web.archive.org/web/20130926170259/http://compsoc.dur.ac.uk:80/archives/whitespace/2010-May/000075.html
 #
 # HTTP Date                  URL                                                                                           Path
 # 2011-02-12 01:57:25 +0000  https://web.archive.org/web/20110212015726/http://hapyli.webs.com:80/                         Tutorials/web/index.html
@@ -108,13 +88,43 @@ https://esolangs.org/wiki/User:Marinus/Brainfuck_interpreters#HaPyLi'
 # 2011-02-19 15:50:17 +0000  https://web.archive.org/web/20110219155019/http://hapyli.webs.com:80/variablesandtheheap.htm  Tutorials/web/Variables.html
 # 2011-02-19 16:25:25 +0000  https://web.archive.org/web/20110219162526/http://hapyli.webs.com:80/embeddingwhitespace.htm  Tutorials/web/Assembler.html
 # 2012-07-12 02:48:54 +0000  https://web.archive.org/web/20120712024855/http://hapyli.webs.com:80/apps/forums/
-cp -R ../../../files/cybis/hapyli/Tutorials/web/*.html Tutorials/web/
-git add Tutorials/web
+cp -Rp ../../../files/cybis/hapyli/2010-05-23/ .
+git add -A
 GIT_AUTHOR_NAME='Kevin' GIT_AUTHOR_EMAIL='cybis-fdp@hotmail.com' GIT_AUTHOR_DATE='2010-05-23 02:20:21 +0000' \
-GIT_COMMITTER_NAME='Kevin' GIT_COMMITTER_EMAIL='cybis-fdp@hotmail.com' GIT_COMMITTER_DATE='2011-02-12 01:57:25 +0000' \
-git commit -q -m 'Release publicly
+GIT_COMMITTER_NAME='Kevin' GIT_COMMITTER_EMAIL='cybis-fdp@hotmail.com' GIT_COMMITTER_DATE='2010-05-23 02:20:21 +0000' \
+git commit -q -m 'Publish tutorial
 
 https://web.archive.org/web/20110212015726/http://hapyli.webs.com:80/
 https://web.archive.org/web/20130926170259/http://compsoc.dur.ac.uk:80/archives/whitespace/2010-May/000075.html'
+
+# 2010-11-27  Marinus Oosters  https://www.99-bottles-of-beer.net/language-hapyli-2556.html
+# Marinus appears to live in CET +0100 / CEST +0200 based on commits from https://github.com/marinuso
+cp -p ../hapyli-svn-checkout/99bottles.hpl .
+diff 99bottles.hpl "$(get_cached_path https://www.99-bottles-of-beer.net/download/2556)"
+git add 99bottles.hpl
+GIT_AUTHOR_NAME='Marinus Oosters' GIT_AUTHOR_EMAIL='marinuso@gmail.com' GIT_AUTHOR_DATE='2010-11-27 00:00:00 +0000' \
+GIT_COMMITTER_NAME='Marinus Oosters' GIT_COMMITTER_EMAIL='marinuso@gmail.com' GIT_COMMITTER_DATE='2010-11-27 00:00:00 +0000' \
+git commit -q -m 'Create 99 bottles of beer in HaPyLi
+
+https://www.99-bottles-of-beer.net/language-hapyli-2556.html'
+
+# brainfuck.hpl was manually extracted from the wiki.
+# 2010-12-01 07:54  Marinus  https://esolangs.org/w/index.php?title=User:Marinus/Brainfuck_interpreters&diff=20310&oldid=18866
+cp -Rp ../../../files/cybis/hapyli/2010-12-01/ .
+git add -A
+GIT_AUTHOR_NAME='Marinus Oosters' GIT_AUTHOR_EMAIL='marinuso@gmail.com' GIT_AUTHOR_DATE='2010-12-01 08:54:00 +0100' \
+GIT_COMMITTER_NAME='Marinus Oosters' GIT_COMMITTER_EMAIL='marinuso@gmail.com' GIT_COMMITTER_DATE='2010-12-01 08:54:00 +0100' \
+git commit -q -m 'Create Brainfuck interpreter in HaPyLi
+
+https://esolangs.org/wiki/User:Marinus/Brainfuck_interpreters#HaPyLi'
+
+cp -Rp ../../../files/cybis/hapyli/2010-12-18/ .
+cp -p ../hapyli-svn-checkout/99bottles.ws .
+git add -A
+GIT_AUTHOR_NAME='Kevin' GIT_AUTHOR_EMAIL='cybis-fdp@hotmail.com' GIT_AUTHOR_DATE='2010-12-18 22:27:46 +0000' \
+GIT_COMMITTER_NAME='Kevin' GIT_COMMITTER_EMAIL='cybis-fdp@hotmail.com' GIT_COMMITTER_DATE='2010-12-18 22:27:46 +0000' \
+git commit -q -m 'List 99 bottles in contributions
+
+https://web.archive.org/web/20110217145734/http://hapyli.webs.com:80/examples.htm'
 
 git remote add origin https://github.com/wspace/cybis-hapyli
