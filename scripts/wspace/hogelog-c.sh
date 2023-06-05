be6afaf83b2751c50efc119ce929e737a5b6bd6c
Thalia Archibald <thalia@archibald.dev>
2023-06-05 17:20:23 -0600

    Partially migrate to git config for authors

--- scripts/wspace/hogelog-c.sh
+++ scripts/wspace/hogelog-c.sh
@@ -14,9 +14,9 @@ mv ws ws_20080502
 git init -q ws_20080502
 git -C ws_20080502 add -A
 
-GIT_AUTHOR_NAME='hogelog' GIT_AUTHOR_EMAIL='konbu.komuro@gmail.com' GIT_AUTHOR_DATE='2008-04-24 18:30:51 +0000 UTC' \
-GIT_COMMITTER_NAME='hogelog' GIT_COMMITTER_EMAIL='konbu.komuro@gmail.com' GIT_COMMITTER_DATE='2008-04-24 18:30:51 +0000 UTC' \
-git -C ws_20080502 commit -q -m 'Initial commit
+GIT_AUTHOR_DATE='2008-04-24 18:30:51 +0000 UTC' GIT_COMMITTER_DATE='2008-04-24 18:30:51 +0000 UTC' \
+git -C ws_20080502 -c user.name='hogelog' -c user.email='konbu.komuro@gmail.com' \
+  commit -q -m 'Initial commit
 
 https://github.com/hogelog/hogel.org-old/blob/master/content/lib/c/ws_20080502.tar.bz2'
 
@@ -25,9 +25,9 @@ mv ws hogelog-c
 mv ws_20080502/.git hogelog-c/
 git -C hogelog-c add -A
 
-GIT_AUTHOR_NAME='hogelog' GIT_AUTHOR_EMAIL='konbu.komuro@gmail.com' GIT_AUTHOR_DATE='2008-05-02 19:24:43 +0000 UTC' \
-GIT_COMMITTER_NAME='hogelog' GIT_COMMITTER_EMAIL='konbu.komuro@gmail.com' GIT_COMMITTER_DATE='2008-05-02 19:24:43 +0000 UTC' \
-git -C hogelog-c commit -q -m 'Rename wsparse.def to parse.def, enable GNU extensions
+GIT_AUTHOR_DATE='2008-05-02 19:24:43 +0000 UTC' GIT_COMMITTER_DATE='2008-05-02 19:24:43 +0000 UTC' \
+git -C hogelog-c -c user.name='hogelog' -c user.email='konbu.komuro@gmail.com' \
+  commit -q -m 'Rename wsparse.def to parse.def, enable GNU extensions
 
 https://github.com/hogelog/hogel.org-old/blob/master/content/lib/c/ws_20080503.tar.bz2'
 
