b0909278c818d8933baaf71f5c2b756d01007d81
Thalia Archibald <thalia@archibald.dev>
2023-06-04 19:26:27 -0600

    martinandhels-whitespacesdk: Convert SVN repo

--- scripts/wspace/kreutzer-spacedide.sh
+++ scripts/wspace/kreutzer-spacedide.sh
@@ -5,17 +5,18 @@
 mkdir -p wspace
 cd wspace
 
-git svn clone --stdlayout https://svn.code.sf.net/p/spacedide/code kreutzer-spacedide
+git svn clone https://svn.code.sf.net/p/spacedide/code kreutzer-spacedide \
+  --stdlayout \
+  --authors-file <(echo '
+spacedide = Sebastian Kreutzer <spacedide@users.sourceforge.net>
+allura = Apache Allura <allura>
+')
 cd kreutzer-spacedide
 
 git filter-repo -f --quiet \
   --message-callback '
     message = re.sub(br"^([\s\S]*?)\n*git-svn-id: .*@(\d+) 253eaa7e-a5eb-43a2-a586-e43a4db1c6d0\n$", br"[r\2] \1\n", message)
     message = re.sub(br"^(\[r\d+\])\s+$", br"\1\n", message)
-    return message' \
-  --mailmap <(echo '
-Sebastian Kreutzer <spacedide@users.sourceforge.net> spacedide <spacedide@253eaa7e-a5eb-43a2-a586-e43a4db1c6d0>
-Apache Allura <allura> allura <allura@253eaa7e-a5eb-43a2-a586-e43a4db1c6d0>
-')
+    return message'
 
 git remote add origin https://github.com/wspace/kreutzer-spacedide
