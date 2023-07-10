b939d9ae649b600f9358560ad57febfb022c6d27
Thalia Archibald <thalia@archibald.dev>
2023-07-09 20:15:12 -0600

    Move SWH archives to swh/

--- scripts/foones/cratylus.sh
+++ scripts/foones/cratylus.sh
@@ -12,7 +12,7 @@ cd foones
 # $ curl -X POST https://archive.softwareheritage.org/api/1/vault/git-bare/swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736/
 # (API docs: https://archive.softwareheritage.org/api/1/vault/git-bare/doc/)
 
-tar xf ../../files/foones/cratylus/swh_1_rev_92066f375c6363830e7e6f83be36b76cefa8d736.git.tar
+tar xf ../../swh/github.com/foones/cratylus/swh_1_rev_92066f375c6363830e7e6f83be36b76cefa8d736.git.tar
 git clone -q swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736.git cratylus
 rm -rf swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736.git
 
