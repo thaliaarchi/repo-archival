0829994e595506e2d1489765bc829f288936fcb9
Thalia Archibald <thalia@archibald.dev>
2023-07-09 21:47:05 -0600

    foones: Incorporate changes from lenguajes

--- scripts/foones/cratylus.sh
+++ scripts/foones/cratylus.sh
@@ -13,9 +13,27 @@ cd foones
 # (API docs: https://archive.softwareheritage.org/api/1/vault/git-bare/doc/)
 
 tar xf ../../swh/github.com/foones/cratylus/swh_1_rev_92066f375c6363830e7e6f83be36b76cefa8d736.git.tar
-git clone -q swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736.git cratylus
+git clone -q swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736.git cratylus-base
 rm -rf swh:1:rev:92066f375c6363830e7e6f83be36b76cefa8d736.git
 
+copy_submodule github.com/foones/lenguajes cratylus
 cd cratylus
+git filter-repo --quiet --subdirectory-filter cratylus
+
+# Rewrite root commit of the filtered lenguajes to have cratylus from SWH as its
+# parent. (git filter-repo cannot be used for this, because it would replace the
+# root commit, instead of changing its parent.)
+git remote add base ../cratylus-base
+git fetch -q base
+FILTER_BRANCH_SQUELCH_WARNING=1 \
+git filter-branch --parent-filter "sed 's/^$/-p $(git rev-parse base/master)/'" master
+git remote remove base
+rm -rf ../cratylus-base
+
+# Restore the deleted .gitignore
+git checkout HEAD~ -- .gitignore
+GIT_COMMITTER_NAME="$(git show -s --format=%cn)" GIT_COMMITTER_EMAIL="$(git show -s --format=%ce)" GIT_COMMITTER_DATE="$(git show -s --format=%cd)" \
+git commit -q --amend --no-edit
+
 git branch -m master main
-git remote set-url origin https://github.com/thaliaarchi/foones-cratylus
+git remote add origin https://github.com/thaliaarchi/foones-cratylus
--- scripts/foones/eightfold.sh
+++ scripts/foones/eightfold.sh
@@ -13,9 +13,24 @@ cd foones
 # (API docs: https://archive.softwareheritage.org/api/1/vault/git-bare/doc/)
 
 tar xf ../../swh/github.com/foones/eightfold/swh_1_rev_f46c932ffb7e88346594ca667cc3758ef27a5692.git.tar
-git clone -q swh:1:rev:f46c932ffb7e88346594ca667cc3758ef27a5692.git eightfold
+git clone -q swh:1:rev:f46c932ffb7e88346594ca667cc3758ef27a5692.git eightfold-base
 rm -rf swh:1:rev:f46c932ffb7e88346594ca667cc3758ef27a5692.git
 
+copy_submodule github.com/foones/lenguajes eightfold
 cd eightfold
+git filter-repo --quiet \
+  --subdirectory-filter eightfold \
+  --replace-message <(echo 'Added Lumpen==>Moved Makefile')
+
+# Rewrite root commit of the filtered lenguajes to have eightfold from SWH as
+# its parent. (git filter-repo cannot be used for this, because it would replace
+# the root commit, instead of changing its parent.)
+git remote add base ../eightfold-base
+git fetch -q base
+FILTER_BRANCH_SQUELCH_WARNING=1 \
+git filter-branch --parent-filter "sed 's/^$/-p $(git rev-parse base/master)/'" master
+git remote remove base
+rm -rf ../eightfold-base
+
 git branch -m master main
-git remote set-url origin https://github.com/thaliaarchi/foones-eightfold
+git remote add origin https://github.com/thaliaarchi/foones-eightfold
