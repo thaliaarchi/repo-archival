4a86cf72ccf58df268efd7f08533736e96dcd6ae
Thalia Archibald <thalia@archibald.dev>
2023-07-29 18:48:12 -0600

    nand2tetris: Start archiving

--- get_ia_date.sh
+++ get_ia_date.sh
@@ -4,9 +4,9 @@ url="$1"
 
 IFS=$'\n'
 
-for header in $(curl -I "$url" | rg 'x-archive-orig-(date|last-modified)'); do
+for header in $(curl -Is "$url" | rg 'x-archive-orig-(date|last-modified)'); do
   key="${header%%: *}"
   value="${header#*: }"
   echo -n "$key: "
-  gdate +'%Y-%m-%d %H:%M:%S %z' -d "$value"
+  TZ="${TZ-UTC}" gdate +'%Y-%m-%d %H:%M:%S %z' -d "$value"
 done
