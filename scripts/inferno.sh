104f1748ea9e7751457c18d5aaa9cc9fefc7ad8b
Thalia Archibald <thalia@archibald.dev>
2024-01-18 04:54:08 -0800

    inferno: List Inferno releases

---
+++ scripts/inferno.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+. base.sh
+
+mkdir -p inferno
+cd inferno
+
+# Vita Nuova distributes Inferno. This page links to four snapshots on Bitbucket
+# and two archives with Mercurial history and untracked binaries. The quotes
+# below come from this page.
+# https://www.vitanuova.com/inferno/downloads.html
+
+# https://github.com/inferno-os/inferno-1e0 (identical mirror)
+# https://bitbucket.org/inferno-os/inferno-1e0/
+# > a beta 1997 release of Inferno 1.0 Build 1 from Lucent's Inferno Business Unit
+# > no non-Limbo source (they didn't include it)
+# > no Plan 9 support
+# > `Distributed Sports Program' demo (with source and images)
+
+copy_submodule github.com/inferno-os/inferno-1e0
+# Two spurious commits add, then delete .gitignore
+git filter-repo --quiet --invert-path --path .gitignore
+
+# https://github.com/inferno-os/inferno-1e1 (identical mirror)
+# https://bitbucket.org/inferno-os/inferno-1e1/
+#
+# > Vita Nuova's modified copy of `BETA.2 Build 16' from Lucent's Inferno Business Unit
+# > includes source
+# > the tree was received in 1996 but this snapshot includes changes made by VN and others into 1997
+
+# https://github.com/inferno-os/inferno-2e (identical mirror)
+# https://bitbucket.org/inferno-os/inferno-2e/
+#
+# > Inferno Second Edition: last distribution (Release 2.3, ca. July 1999) from Lucent's Inferno Business Unit, including source
+# > no manual pages?? exactly.
+# > the documents are not included; they were in Framemaker format on Nt or Solaris
+
+# https://github.com/inferno-os/inferno-3e (identical mirror)
+# https://bitbucket.org/inferno-os/inferno-3e/
+#
+# > source-only distribution of the last Inferno Third Edition (20030603)
+
+# Inferno Fourth Edition 20150328 snapshot:
+#
+# > Unix-like systems
+# > (FreeBSD, Linux, MacOS X, Plan 9)
+# > A snapshot of the complete source tree, including executables such as mk for
+# > its reconstruction and the full set of fonts, is available as a gzip'd tar
+# > file inferno-20150328.tgz. Download and unpack the archive; note that the
+# > contents of the archive is rooted at inferno. There are installation
+# > instructions in the file INSTALL. It tells how to change mkconfig and set
+# > your path or PATH to the right bin directory before you can remake the tree
+# > using mk.
+#
+# https://www.vitanuova.com/dist/4e/inferno-20150328.tgz
+
+# > Windows 2000, XP, and 7
+# > A snapshot of the complete source tree, including executables such as mk for
+# > its reconstruction, the full set of fonts, and pre-compiled Windows
+# > executables and libraries is available from this Vita Nuova site as a ZIP
+# > archive inferno.zip. The archive contains a single folder inferno which
+# > contains the Inferno distribution (and executables etc.) including the
+# > Mercurial repository.
+#
+# https://www.vitanuova.com/dist/4e/inferno.zip
+
+# https://github.com/inferno-os/inferno-os (current)
+# https://bitbucket.org/inferno-os/inferno-os/ (old)
+#
+# Current Inferno development
+#
+# The Bitbucket repo used to be the main source, but development seems to now
+# have switched to GitHub. The last common commit was on 2021-10-11 and any
+# commits after that on Bitbucket are cherry-picked from GitHub.
+
+# Miscellaneous repos, that are not on both forges:
+#
+# - https://bitbucket.org/inferno-os/java2dis/: Java class file execution in
+#   Inferno's Dis VM
+# - https://bitbucket.org/inferno-os/browser-plugin/: Inferno as an old IE
+#   browser plugin
+# - https://bitbucket.org/inferno-os/owen/: Computational grid
+# - https://github.com/inferno-os/ventivac: Venti components for Inferno (GSoC)
+# - https://bitbucket.org/inferno-os/inferno64-os/: History is fully included in
+#   inferno-os/inferno-os, so it seems to serve no purpose
