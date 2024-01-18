7c395eb88776542b7847eb043b45aae9a4ead9e0
Thalia Archibald <thalia@archibald.dev>
2024-01-18 06:12:48 -0800

    inferno: Convert inferno-os-hg to git and verify

--- scripts/minecraft/cuchaz-mappings.sh
+++ scripts/minecraft/cuchaz-mappings.sh
@@ -6,6 +6,8 @@ mkdir -p minecraft
 cd minecraft
 mkdir cuchaz-mappings cuchaz-mappings-hg
 
+# https://archive.softwareheritage.org/browse/origin/directory/?origin_url=https://bitbucket.org/cuchaz/minecraft-mappings
+# https://bitbucket-archive.softwareheritage.org/projects/cu/cuchaz/minecraft-mappings.html
 tar xf "$(get_cached_path https://bitbucket-archive.softwareheritage.org/new-static/c1/c1fd27e7-2e49-4594-91d2-0816b7c5a6dd/c1fd27e7-2e49-4594-91d2-0816b7c5a6dd-repository.tar.gz)" \
   --strip-components=1 \
   -C cuchaz-mappings-hg
@@ -15,8 +17,6 @@ git init -q
 git config core.ignoreCase false
 # https://github.com/frej/fast-export
 hg-fast-export -r ../cuchaz-mappings-hg -M main
-git checkout HEAD
+git checkout -q HEAD
 
 rm -r ../cuchaz-mappings-hg
-
-git remote add origin https://github.com/thaliaarchi/cuchaz-minecraft-mappings
