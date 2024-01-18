90e989a30049787af440e69f5cc8674f4dbf7c7b
Thalia Archibald <thalia@archibald.dev>
2024-01-18 09:57:42 -0800

    Use Mercurial work tree, when converting to git

--- scripts/minecraft/cuchaz-mappings.sh
+++ scripts/minecraft/cuchaz-mappings.sh
@@ -4,13 +4,12 @@
 
 mkdir -p minecraft
 cd minecraft
-mkdir cuchaz-mappings cuchaz-mappings-hg
+mkdir cuchaz-mappings
 
 # https://archive.softwareheritage.org/browse/origin/directory/?origin_url=https://bitbucket.org/cuchaz/minecraft-mappings
 # https://bitbucket-archive.softwareheritage.org/projects/cu/cuchaz/minecraft-mappings.html
 tar xf "$(get_cached_path https://bitbucket-archive.softwareheritage.org/new-static/c1/c1fd27e7-2e49-4594-91d2-0816b7c5a6dd/c1fd27e7-2e49-4594-91d2-0816b7c5a6dd-repository.tar.gz)" \
   --strip-components=1 \
-  -C cuchaz-mappings-hg
-
-hg_to_git cuchaz-mappings-hg cuchaz-mappings
-rm -r cuchaz-mappings-hg
+  -C cuchaz-mappings
+hg_to_git cuchaz-mappings
+cd cuchaz-mappings
