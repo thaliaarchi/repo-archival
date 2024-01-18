8557ac4aec96960b3f9d5415291ca9bb20908b19
Thalia Archibald <thalia@archibald.dev>
2024-01-18 04:16:10 -0800

    Clone from submodules instead of copying and fixing

--- base.sh
+++ base.sh
@@ -15,44 +15,9 @@ if [[ ! -e .git ]]; then
 fi
 
 copy_submodule() {
-  local submodule="git/$1"
+  local submodule="$1"
   local dest="${2-"${submodule##*/}"}"
-
-  echo "Copying submodule $submodule to target/$dest"
-  if [[ -e $dest ]]; then
-    echo "Destination exists: $dest" >&2
-    return 1
-  fi
-  mkdir -p "$(dirname "$dest")"
-  cp -rp "$TOPLEVEL/$submodule" "$dest"
-
-  # If the submodule had its git dir absorbed, copy it from .git/modules
-  local git_file git_dir
-  git_file="$TOPLEVEL/$submodule/.git"
-  git_dir="$(git --git-dir "$git_file" rev-parse --absolute-git-dir)"
-  if [[ "$git_file" != "$git_dir" ]]; then
-    rm -rf "$dest/.git"
-    cp -rp "$git_dir" "$dest/.git"
-    sed -i.bak '/worktree =/d' "$dest/.git/config"
-    rm "$dest/.git/config.bak"
-  fi
-
-  # If the repo has a detached HEAD, attempt to checkout a branch pointing to it
-  local current_branch branch_at_head
-  current_branch="$(git --git-dir "$dest/.git" branch --show-current)"
-  if [[ -z $current_branch ]]; then
-    branch_at_head="$(git --git-dir "$dest/.git" for-each-ref refs/heads --points-at HEAD --format='%(refname:lstrip=2)' | head -n1)"
-    if [[ -n "$branch_at_head" ]]; then
-      git --git-dir "$dest/.git" checkout -q "$branch_at_head"
-    else
-      echo "$submodule: No branch points to HEAD. Maybe the submodule is out of sync?" >&2
-      return 1
-    fi
-  fi
-
-  # Allow running git filter-repo without --force
-  mkdir -p "$dest/.git/filter-repo"
-  touch "$dest/.git/filter-repo/already_ran"
+  git clone -q "$TOPLEVEL/git/$submodule/.git" "$dest"
 }
 
 merge_repo() {
