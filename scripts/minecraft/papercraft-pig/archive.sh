693c4c50cabb246b45769dd565308fe77e6e9be3
Thalia Archibald <thalia@archibald.dev>
2024-02-09 03:03:08 -0800

    papercraft-pig: Commit files from zip by date modified

--- scripts/minecraft/papercraft-pig/archive.sh
+++ scripts/minecraft/papercraft-pig/archive.sh
@@ -16,8 +16,23 @@ commit_as_ppa() {
 
 mkdir -p minecraft
 cd minecraft
-mkdir papercraft-pig papercraft-pig-emails pig-advanced
-cd papercraft-pig-emails
+mkdir papercraft-pig pig-advanced
+
+# This release of the code is from 2015-03-09 (see that commit below for details
+# on its metadata). Its files are modified closely in line with the emails, so I
+# commit the files in chunks according to when they were modified in the zip.
+# This helps to supplement the few known revisions with extant code.
+#
+# Note, that zips use local times, but do not store the timezone, so it extracts
+# in your local timezone when you extract it. That means that if a zip is
+# extracted by someone else, modified, then re-zipped, then any unchanged files
+# will have the same time in the zip as before (i.e., local to the original zip
+# creator's timezone) and any modified files will be local to the other person's
+# timezone. Timezones get messy when zips are passed around, but at least the
+# times always remain the same. Thus, when assigning timezones to files in a
+# zip, it varies by file according to the authorship of the file.
+TZ=US/Pacific unzip -q "$(get_cached_path https://web.archive.org/web/20150326203916/http://pixelpapercraft.com/app/generators/minecraft/pig-advanced.zip)"
+cd pig-advanced
 git init -q
 
 # Sources:
@@ -66,6 +81,15 @@ git init -q
 # This version of the code was not sent. I include the email as the commit body
 # to provide some context for what features it had, since it had no changelog
 # entry. The commit summary line is synthesized.
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2014-11-24 15:08:14 (local)  pig-advanced/texture/vanilla/chainmail-armor.png
+# 2014-11-24 15:08:14 (local)  pig-advanced/texture/vanilla/diamond-armor.png
+# 2014-11-24 15:08:14 (local)  pig-advanced/texture/vanilla/gold-armor.png
+# 2014-11-24 15:08:14 (local)  pig-advanced/texture/vanilla/iron-armor.png
+# 2014-12-30 09:23:22 (local)  pig-advanced/texture/vanilla/pig.png
+# 2014-12-30 09:23:46 (local)  pig-advanced/texture/vanilla/saddle.png
+git add texture/vanilla/{chainmail-armor.png,diamond-armor.png,gold-armor.png,iron-armor.png,pig.png,saddle.png}
 commit_as_tepigmc '2015-01-01 15:58 -0800' 'Papercraft generator for pigs
 
 It has options to enable helmets, boots, and saddles that can be
@@ -118,6 +142,21 @@ commit_as_tepigmc '2015-01-06 10:18 -0800' 'Sent pig generator'
 # > builder? And yes I have used JavaScript before. I am fluent in JavaScript,
 # > AngularJS, and jQuery.
 
+# No email or changelog entry
+#
+# These textures were most likely downloaded just for the pig generator, since I
+# don't remember ever playing Minecraft with these texture packs. Rather than
+# attribute them to a later commit by me, I make this revision, because it shows
+# I was working on it this day. The message is synthetic.
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-01-11 15:30:02 (local)  pig-advanced/texture/space-pig/pig.png
+# 2015-01-14 16:13:06 (local)  pig-advanced/texture/faithful/pig.png
+# 2015-01-14 16:13:06 (local)  pig-advanced/texture/faithful/saddle.png
+# 2015-01-14 16:13:40 (local)  pig-advanced/texture/faithful/diamond-armor.png
+git add texture/{faithful/{diamond-armor.png,pig.png,saddle.png},space-pig/pig.png}
+commit_as_tepigmc '2015-01-14 16:13:40 -0800' 'Added Faithful and Space Pig textures'
+
 # Email #6 from PPA on 2015-02-07 08:39 +1100
 #
 # > You've done an amazing job with your Pig Generator and the ultra mini pig is
@@ -142,8 +181,10 @@ commit_as_tepigmc '2015-01-06 10:18 -0800' 'Sent pig generator'
 # > Always great to see people working with JavaScript. Pixel Papercraft is
 # > actually written almost entirely in JS with Node.js on the server and
 # > Backbone.JS on the front end.
+mv Script.txt{,.bak}
 cp -p "$TOPLEVEL/files/minecraft/papercraft-pig/Script.txt" .
 git add Script.txt
+mv Script.txt{.bak,}
 commit_as_ppa '2015-02-07 08:39 +1100' 'Add user variables'
 
 # Email #7 from PPA on 2015-02-07 08:43:53 +1100
@@ -196,8 +237,6 @@ commit_as_tepigmc '2015-02-26 17:19 -0800' 'Removed missing texture checks; Rena
 # > If you think it's OK then I'll officially announce it on the blog.
 # >
 # > http://pixelpapercraft.com/print/minecraft-pig-advanced
-#
-# Synthesized commit message
 commit_as_ppa '2015-02-27 18:42:06 +1100' 'Release generator to the site'
 
 # Email #11 from TepigMC on 2015-02-27 20:21 -0800
@@ -208,9 +247,38 @@ commit_as_ppa '2015-02-27 18:42:06 +1100' 'Release generator to the site'
 # > CSS sprites. That way there won’t be 20 or so sprites used. I have made lots
 # > of changes, so don’t post it in the news yet.
 #
-# I don't think this day's revision from the changelog was ever sent, so the
-# email doesn't necessarily correspond to when this revision was made.
-commit_as_tepigmc '2015-02-27 20:21 -0800' 'Compacted backgrounds and folds into sprite files'
+# I don't think this day's code was ever sent, although it has an entry in the
+# changelog, so the email doesn't necessarily correspond to when this revision
+# was made. There are more files revised on this day (see the next commit), but
+# they are named in the next revision, so I move them to then; there's 1.5 hours
+# between too.
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-02-27 16:15:02 (local)  pig-advanced/image/seperated/backgrounds/nose3D.png
+# 2015-02-27 16:15:28 (local)  pig-advanced/image/seperated/backgrounds/leg.png
+# 2015-02-27 16:17:12 (local)  pig-advanced/image/seperated/backgrounds/headAdvanced.png
+# 2015-02-27 16:17:38 (local)  pig-advanced/image/seperated/backgrounds/helmet.png
+# 2015-02-27 16:18:00 (local)  pig-advanced/image/seperated/backgrounds/body.png
+# 2015-02-27 16:18:16 (local)  pig-advanced/image/seperated/backgrounds/ultraMini.png
+# 2015-02-27 16:18:32 (local)  pig-advanced/image/seperated/titles/pixelPapercraft.png
+# 2015-02-27 16:18:38 (local)  pig-advanced/image/seperated/titles/tepigmc.png
+# 2015-02-27 16:27:00 (local)  pig-advanced/image/seperated/backgrounds/headSimple.png
+# 2015-02-27 16:58:16 (local)  pig-advanced/image/seperated/backgrounds/boot.png
+# 2015-02-27 17:19:36 (local)  pig-advanced/image/seperated/backgrounds/opaque.png
+# 2015-02-27 20:39:54 (local)  pig-advanced/image/seperated/titles/ultraMini.png
+# 2015-02-27 20:40:24 (local)  pig-advanced/image/seperated/titles/leg.png
+# 2015-02-27 20:48:14 (local)  pig-advanced/image/seperated/folds/body.png
+# 2015-02-27 20:52:26 (local)  pig-advanced/image/seperated/folds/nose3D.png
+# 2015-02-27 20:53:10 (local)  pig-advanced/image/seperated/folds/leg.png
+# 2015-02-27 20:54:00 (local)  pig-advanced/image/seperated/folds/boot.png
+# 2015-02-27 20:56:22 (local)  pig-advanced/image/seperated/folds/saddle.png
+# 2015-02-27 20:57:58 (local)  pig-advanced/image/seperated/folds/headAdvanced.png
+# 2015-02-27 20:59:08 (local)  pig-advanced/image/seperated/folds/headSimple.png
+# 2015-02-27 21:01:14 (local)  pig-advanced/image/seperated/folds/headAdvancedCuts.png
+git add image/seperated/backgrounds/{body.png,boot.png,headAdvanced.png,headSimple.png,helmet.png,leg.png,nose3D.png,opaque.png,ultraMini.png} \
+        image/seperated/folds/{body.png,boot.png,headAdvanced.png,headAdvancedCuts.png,headSimple.png,leg.png,nose3D.png,saddle.png} \
+        image/seperated/titles/{leg.png,pixelPapercraft.png,tepigmc.png,ultraMini.png}
+commit_as_tepigmc '2015-02-27 21:01:14 -0800' 'Compacted backgrounds and folds into sprite files'
 
 # Email #12 from TepigMC on 2015-02-28 12:21 -0800
 #
@@ -229,9 +297,43 @@ commit_as_tepigmc '2015-02-27 20:21 -0800' 'Compacted backgrounds and folds into
 # > update the description to match the readme. I also posted another image that
 # > I would like linked to the generator.
 #
-# Although a revision was sent, it was retracted. Rather than synthesizing a
-# commit message, I just omit this one and use the day's changelog entry for the
-# finished revision.
+# Although a revision was sent, it was retracted. However, there is an hour
+# break between the files for this revision and the next, so I include it. The
+# message from the changelog seems to split neatly between the files changed in
+# this and the next revision.
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-02-27 22:25:56 (local)  pig-advanced/image/seperated/labels/headNose3D.png
+# 2015-02-27 22:28:18 (local)  pig-advanced/image/seperated/labels/leg1.png
+# 2015-02-27 22:28:22 (local)  pig-advanced/image/seperated/labels/leg2.png
+# 2015-02-27 22:28:30 (local)  pig-advanced/image/seperated/labels/leg3.png
+# 2015-02-27 22:28:44 (local)  pig-advanced/image/seperated/labels/leg4.png
+# 2015-02-28 08:43:30 (local)  pig-advanced/image/seperated/titles/head.png
+# 2015-02-28 08:44:14 (local)  pig-advanced/image/seperated/labels/head.png
+# 2015-02-28 08:44:42 (local)  pig-advanced/image/seperated/labels/nose3D.png
+# 2015-02-28 08:45:10 (local)  pig-advanced/image/seperated/titles/nose3D.png
+# 2015-02-28 08:45:40 (local)  pig-advanced/image/seperated/titles/body.png
+# 2015-02-28 08:46:12 (local)  pig-advanced/image/seperated/labels/bodyHead.png
+# 2015-02-28 08:48:08 (local)  pig-advanced/image/seperated/labels/bodyLeg1.png
+# 2015-02-28 08:48:14 (local)  pig-advanced/image/seperated/labels/bodyLeg2.png
+# 2015-02-28 08:48:28 (local)  pig-advanced/image/seperated/labels/bodyLeg3.png
+# 2015-02-28 08:48:34 (local)  pig-advanced/image/seperated/labels/bodyLeg4.png
+# 2015-02-28 08:49:22 (local)  pig-advanced/image/seperated/titles/boot.png
+# 2015-02-28 08:49:34 (local)  pig-advanced/image/seperated/titles/saddle.png
+# 2015-02-28 08:49:42 (local)  pig-advanced/image/seperated/titles/helmet.png
+# 2015-02-28 10:10:38 (local)  pig-advanced/image/seperated/folds/headStandardAdvanced.png
+# 2015-02-28 10:11:10 (local)  pig-advanced/image/seperated/backgrounds/headStandardAdvanced.png
+# 2015-02-28 10:20:30 (local)  pig-advanced/image/seperated/labels/headStandardAdvanced.png
+# 2015-02-28 10:27:48 (local)  pig-advanced/image/label-sprites.png
+# 2015-02-28 10:40:52 (local)  pig-advanced/image/title-sprites.png
+# 2015-02-28 10:43:06 (local)  pig-advanced/image/background-sprites.png
+# 2015-02-28 12:05:26 (local)  pig-advanced/image/Readme.md
+git add image/{background-sprites.png,label-sprites.png,Readme.md,title-sprites.png} \
+        image/seperated/backgrounds/headStandardAdvanced.png \
+        image/seperated/folds/headStandardAdvanced.png \
+        image/seperated/labels/{bodyHead.png,bodyLeg1.png,bodyLeg2.png,bodyLeg3.png,bodyLeg4.png,head.png,headNose3D.png,headStandardAdvanced.png,leg1.png,leg2.png,leg3.png,leg4.png,nose3D.png} \
+        image/seperated/titles/{body.png,boot.png,head.png,helmet.png,nose3D.png,saddle.png}
+commit_as_tepigmc '2015-02-28 12:21 -0800' 'Compacted labels and titles into sprite files; Added "Advanced (Standard)" head'
 
 # Email #13 from TepigMC on 2015-02-28 13:08 -0800
 #
@@ -256,8 +358,23 @@ commit_as_tepigmc '2015-02-27 20:21 -0800' 'Compacted backgrounds and folds into
 # > empty `<option>` at the beginning no matter what. Is that intentional or an
 # > error? Isn’t it supposed to show the option that is selected? Also, if you
 # > are willing, it would be nice to add support for `<optgroup>` in the select.
-commit_as_tepigmc '2015-02-28 16:59 -0800' 'Compacted labels and titles into sprite files; Added "Advanced (Standard)" head
-Added "Show Helmet Overlay" option; Added texture options'
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-02-28 13:01:12 (local)  pig-advanced/texture/space-pig/saddle.png
+# 2015-02-28 13:01:44 (local)  pig-advanced/texture/space-pig/armor.png
+# 2015-02-28 13:13:12 (local)  pig-advanced/image/seperated/folds/helmet.png
+# 2015-02-28 13:42:36 (local)  pig-advanced/texture/tepig/elpis.png
+# 2015-02-28 15:16:10 (local)  pig-advanced/image/fold-sprites.png
+# 2015-02-28 15:56:36 (local)  pig-advanced/texture/tepig/audra.png
+# 2015-02-28 16:31:44 (local)  pig-advanced/texture/faithful/chainmail-armor.png
+# 2015-02-28 16:31:44 (local)  pig-advanced/texture/faithful/gold-armor.png
+# 2015-02-28 16:31:44 (local)  pig-advanced/texture/faithful/iron-armor.png
+# 2015-02-28 16:44:32 (local)  pig-advanced/Readme.md
+git add Readme.md image/fold-sprites.png image/seperated/folds/helmet.png \
+        texture/faithful/{chainmail-armor.png,gold-armor.png,iron-armor.png} \
+        texture/space-pig/{armor.png,saddle.png} \
+        texture/tepig/{audra.png,elpis.png}
+commit_as_tepigmc '2015-02-28 16:59 -0800' 'Added "Show Helmet Overlay" option; Added texture options'
 
 # Email #16 from PPA on 2015-03-01 17:04:09 +1100
 #
@@ -290,43 +407,62 @@ Added "Show Helmet Overlay" option; Added texture options'
 # > two.
 #
 # Since this page does not have Last-Modified and IA does not have and earlier
-# revision, I only link to the generator in this commit, not the earlier
-# release. Synthesized commit message.
+# revision, I link to the generator page in this commit, not in the earlier
+# commit. The commit message is synthesized.
+#
+# Resources.js was modified here, just as the email says. Note that I use +1100,
+# because this version was authored by PPA. The times are preserved between
+# extractions; only the timezones are reset.
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/seperated/
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/seperated/backgrounds/
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/seperated/folds/
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/seperated/labels/
+# 2015-03-01 13:58:38 (local)  pig-advanced/image/seperated/titles/
+# 2015-03-01 13:58:38 (local)  pig-advanced/texture/
+# 2015-03-01 13:58:38 (local)  pig-advanced/texture/faithful/
+# 2015-03-01 13:58:38 (local)  pig-advanced/texture/space-pig/
+# 2015-03-01 13:58:38 (local)  pig-advanced/texture/tepig/
+# 2015-03-01 13:58:38 (local)  pig-advanced/texture/vanilla/
+# 2015-03-01 15:20:06 (local)  pig-advanced/Resources.js
+git add Resources.js
 commit_as_ppa '2015-03-01 17:04:09 +1100' 'Release the new version to the site
 
 https://web.archive.org/web/20150302194936/http://pixelpapercraft.com/print/minecraft-pig-advanced'
 
 # The latest contained file, pig-advanced/Script.txt, was modified
-# 2015-03-09 19:38:30 in the local timezone of the creator of the zip. Zips use
-# local times, but do not store the timezone, so it extracts in your local
-# timezone when you extract it. This is incorrect, but what we have.
+# 2015-03-09 19:38:30 in a local timezone. The changelog suggests this was my
+# change, so its timezone is most likely -0800.
 #
 # The HTTP Last-Modified header is 2015-03-09 08:45:11 +0000. Notice that this
-# is 11 hours before Script.txt. This indicates that PPA's copy of the zip had a
-# modification time of 2015-03-09 19:45:11 +1100, which was preserved when they
-# uploaded it to the server.
+# is 11 hours before Script.txt, with an extra 7 minutes.
 #
-# If I made this zip, and the changelog suggests that I did, then the files
-# would be -0800 and I would have emailed it to PPA shortly thereafter (which I
-# no longer have a record of). Many email clients will set the modification time
-# of downloaded attachments to the date the email was sent. Unlike zips, emails
-# have the timezone, so it would have to set it to my time, but skewed to PPA's
-# timezone. Assuming all that, we can say the zip was created at
-# 2015-03-09 19:45:11 -0800.
+# Supposed I modified Script.txt at 2015-03-09 19:38:30 -0800, zipped it up,
+# then emailed it to PPA at 2015-03-09 19:45:11 -0800. Then, when PPA downloaded
+# the attachment, their email client would use the email date as the
+# modification time (at least mine does this). If the client interpreted the
+# email date as local when setting the modification time, then things line up.
+# Problem is, emails have the timezone, so this logic in the email client would
+# have to not be timezone-aware. Assuming that, then PPA's copy of
+# pig-advanced.zip would have a modification time of 2015-03-09 19:45:11 +1100,
+# which would be preserved when uploading it to the server.
 #
-# Alternatively, considering the case that PPA made the zip, then the local
-# times in the zip would be +1100, which matches neatly with HTTP Last-Modified
-# time. The problem is that the changelog records a revision by me on this date
-# and it's highly coincidental otherwise that PPA would have done a release. If
-# the Last-Modified time is not skewed, then this would place my revision before
-# 2015-03-09 00:38:30 -0800. I highly doubt I would have made a commit that late
-# back then, and even if I did, I most likely would have noted it in the
+# Alternatively, if the HTTP Last-Modified is not skewed, then my revision would
+# be before 2015-03-09 00:45:11 -0800. A commit that late would have been rare
+# back then. A small fix like this doesn't seem like the thing I'd stay up to
+# do on its own, and even if I did, I most likely would have noted it in the
 # changelog as the day before.
 #
 # Thus, I use the time of the HTTP Last-Modified header, skewed forward to match
 # the inner modification times, and in -0800.
-TZ=US/Pacific unzip -d .. -q "$(get_cached_path https://web.archive.org/web/20150326203916/http://pixelpapercraft.com/app/generators/minecraft/pig-advanced.zip)"
-git --work-tree=../pig-advanced add -A
+#
+# Files from pig-advanced.zip attributed to this revision:
+# 2015-03-09 19:38:02 (local)  pig-advanced/
+# 2015-03-09 19:38:26 (local)  pig-advanced/Script.js
+# 2015-03-09 19:38:30 (local)  pig-advanced/Script.txt
+git add Script.js Script.txt
 commit_as_tepigmc '2015-03-09 19:45:11 -0800' 'Fixed small bug with ultra mini pig' \
   --trailer=Source:https://web.archive.org/web/20150326203916/http://pixelpapercraft.com/app/generators/minecraft/pig-advanced.zip
 
@@ -342,11 +478,11 @@ git filter-repo \
     commit.message = re.sub(br"(?:\r?\n)+$", b"", commit.message) + b"\n\nSource: https://github.com/pixelpapercraft/pixel-papercraft-generator-builder/commit/" + commit.original_id + b"\n"
   '
 # Graft it onto the synthetic email commits.
-git remote add emails ../papercraft-pig-emails
-git fetch -q emails
-git replace --graft "$(git rev-list --max-parents=0 HEAD)" emails/main
+git remote add base ../pig-advanced
+git fetch -q base
+git replace --graft "$(git rev-list --max-parents=0 HEAD)" base/main
 git filter-repo -f --quiet
-git remote remove emails
+git remote remove base
 
 cd ..
-rm -rf papercraft-pig-emails pig-advanced
+rm -rf pig-advanced
