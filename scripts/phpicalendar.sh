8ae856e2d4dffd506fc14e9394181e5310b3ebc7
Thalia Archibald <thalia@archibald.dev>
2024-03-22 18:44:04 -0700

    phpicalendar: Set LC_ALL=C to use sed with binary data

--- scripts/phpicalendar.sh
+++ scripts/phpicalendar.sh
@@ -6,18 +6,19 @@ set -eEuo pipefail
 mkdir phpicalendar
 cd phpicalendar
 
-# PHP iCalendar (https://sourceforge.net/projects/phpicalendar/)
+# Convert PHP iCalendar from CVS at https://sourceforge.net/projects/phpicalendar/.
 rsync -ai a.cvs.sourceforge.net::cvsroot/phpicalendar/ phpicalendar.cvs
 cd phpicalendar.cvs
 find . -name '*,v' | cvs-fast-export > ../phpicalendar.fe
-cd ../fe-fix-paths
+cd ..
 # The cvs-fast-export stream contains paths starting with `/`, which are
 # rejected by git fast-import with `fatal: Empty path component found in input`.
-# Since sed cannot work with binary files, fix them in Rust:
-cargo run ../phpicalendar.fe > ../phpicalendar.fe.patched
-cd ..
-git init phpicalendar.git
+# Get around that by removing the leading `/`. (I should upstream a proper fix.)
+LC_ALL=C sed -E 's,(^M [0-9]{6} (:[0-9]+|inline) )/,\1,' phpicalendar.fe > phpicalendar.fe.patched
+
+git init -q phpicalendar.git
 cd phpicalendar.git
 git fast-import < ../phpicalendar.fe.patched
+# Normalize line endings to improve diffs for files with CR-only line endings.
 git filter-repo --force --blob-callback 'blob.data = blob.data.replace(b"\r\n", b"\n").replace(b"\r", b"\n")'
-git checkout master
+git checkout -q master
