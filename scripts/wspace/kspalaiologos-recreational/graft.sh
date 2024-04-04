06c620c3e1c5ffe81933aad87d3f11ffc5f05358
Thalia Archibald <thalia@archibald.dev>
2024-04-04 01:44:28 -0700

    kspalaiologos-recreational: Interleave histories

--- scripts/wspace/kspalaiologos-recreational/graft.sh
+++ scripts/wspace/kspalaiologos-recreational/graft.sh
@@ -6,16 +6,11 @@ set -eEuo pipefail
 mkdir -p wspace
 cd wspace
 
-# kspalaiologos' code golf and other programming puzzle solutions lived in
-# https://web.archive.org/web/20230622203235/https://github.com/kspalaiologos/codegolf-submissions,
-# from 2019-08-14 to 2022-03-21, which I've archived at https://github.com/wspace/kspalaiologos-codegolf.
-# She restarted in https://github.com/kspalaiologos/recreational with an empty
-# Git history on 2022-03-02 and copied the codegolf-submissions files into it on
-# 2023-11-12. Graft those histories together.
+# Graft the Git histories.
 
-clone_submodule https://github.com/kspalaiologos/recreational kspalaiologos-recreational
+clone_submodule https://github.com/kspalaiologos/recreational kspalaiologos-recreational.grafted
 codegolf_submodule="$(submodule_path https://github.com/wspace/kspalaiologos-codegolf)"
-cd kspalaiologos-recreational
+cd kspalaiologos-recreational.grafted
 
 git remote add codegolf "$codegolf_submodule"
 git fetch -q codegolf
@@ -28,5 +23,7 @@ git fetch -q codegolf
 git replace --graft b0b31d9e8d56c6986613a3b49762b61a658a9eb8 \
   93b3082bb547296d11411578181aeff928e1531b \
   codegolf/main
+
+# TODO: Graft commit is unsigned.
 
 git remote remove codegolf
