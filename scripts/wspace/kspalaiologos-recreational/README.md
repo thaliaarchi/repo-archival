5f50124535e5e4234a649a7da7d6c1a300cbe8a6
Thalia Archibald <thalia@archibald.dev>
2024-04-04 02:07:00 -0700

    kspalaiologos-recreational: Expand descriptions and rename remote

--- scripts/wspace/kspalaiologos-recreational/README.md
+++ scripts/wspace/kspalaiologos-recreational/README.md
@@ -10,8 +10,14 @@ Git history on 2022-03-02 and copied the codegolf-submissions files into it on
 I have merged the repos in two ways:
 
 - [interleave.sh](./interleave.sh) rebases the repos so their commits are
-  interleaved chronologically into one linear history.
+  interleaved chronologically into one linear history. Any commits after the
+  first commit in recreational will have different hashes and need to be
+  force-pushed.
 
 - [graft.sh](./graft.sh) replaces [1f9b505](https://github.com/kspalaiologos/recreational/commit/1f9b5057f9a9786b37c11f784b3102541ea39668)
   (Update README.md, 2023-12-14) with a merge commit, which is exactly the same,
-  except its second parent is the history of codegolf-submissions.
+  except its second parent is the history of codegolf-submissions. This creates
+  a replace ref, which replaces the original commit with another in operations
+  like `git log` and `git blame`, but only in rendering, not actually the object
+  itself. Replace refs, unfortunately are not fetched from GitHub by other
+  people, even if you pushed them, so no one else would see the old history.
--- scripts/wspace/kspalaiologos-recreational/graft.sh
+++ scripts/wspace/kspalaiologos-recreational/graft.sh
@@ -12,18 +12,22 @@ clone_submodule https://github.com/kspalaiologos/recreational kspalaiologos-recr
 codegolf_submodule="$(submodule_path https://github.com/wspace/kspalaiologos-codegolf)"
 cd kspalaiologos-recreational.grafted
 
-git remote add codegolf "$codegolf_submodule"
-git fetch -q codegolf
+git remote add codegolf-submissions "$codegolf_submodule"
+git fetch -q codegolf-submissions
 
-# The bulk of codegolf was copied into recreational in b0b31d9 (more stuff from
-# cgse, 2023-11-12). Make that commit into into a merge commit, where its first
-# parent remains 93b3082 (Create 41-4.b, 2023-08-24) from recreational and its
-# second parent becomes 85ded3d (Create 239831.apl, 2022-03-21), the last commit
-# of codegolf.
+# The bulk of codegolf-submissions was copied into recreational in b0b31d9 (more
+# stuff from cgse, 2023-11-12). Make that commit into into a merge commit, where
+# its first parent remains 93b3082 (Create 41-4.b, 2023-08-24) from recreational
+# and its second parent becomes 85ded3d (Create 239831.apl, 2022-03-21), the
+# last commit of codegolf-submissions.
+#
+# The first parent is the mainline branch and affects diffs. If you want
+# codegolf-submissions to be the mainline, swap codegolf-submissions/main and
+# 93b3082bb547296d11411578181aeff928e1531b.
 git replace --graft b0b31d9e8d56c6986613a3b49762b61a658a9eb8 \
   93b3082bb547296d11411578181aeff928e1531b \
-  codegolf/main
+  codegolf-submissions/main
 
 # TODO: Graft commit is unsigned.
 
-git remote remove codegolf
+git remote remove codegolf-submissions
