84971cb20252320495696590e90c6757f4ae3a4a
Thalia Archibald <thalia@archibald.dev>
2024-04-26 19:18:07 -0700

    ms-dos: Generate commits for MS-DOS 3.30 and 6.0

--- scripts/ms-dos.sh
+++ scripts/ms-dos.sh
@@ -7,6 +7,7 @@ set -eEuo pipefail
 # - Create linear history with the four versions modifying the same directories.
 # - Use the Computer History Museum's version of v1.25 and v2.0.
 # - Investigate force-pushed commits.
+# - Examine executable file modes.
 # - Pick either the original or optimized PNG version for the logo. Where were
 #   they sourced? The PNGs may have useful text (https://www.w3.org/TR/png-3/#11textinfo),
 #   EXIF (https://www.w3.org/TR/png-3/#eXIf), or timestamp (https://www.w3.org/TR/png-3/#11timestampinfo)
@@ -17,8 +18,42 @@ set -eEuo pipefail
 # https://news.ycombinator.com/item?id=40163405
 # https://www.os2museum.com/wp/how-not-to-release-historic-source-code/
 
+export GIT_AUTHOR_NAME='Microsoft Open Source'
+export GIT_AUTHOR_EMAIL='microsoftopensource@users.noreply.github.com'
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
+commit() {
+  local date="$1"
+  local message="$2"
+  GIT_AUTHOR_DATE="$date" GIT_COMMITTER_DATE="$date" \
+  git commit -q -m "$message"
+}
+
 mkdir ms-dos
 cd ms-dos
+
+git init -q
+git config core.autocrlf false
+
+# The files are modified on two dates:
+# 1987-07-24 05:00:02 +0000 SOURCE/{PROGREF,SRC,AG.DOC,README.BF,README.DIS}/**
+# 1988-02-02 06:00:02 +0000 SOURCE/UPDSRC/**
+7z x "$(get_cached_path https://archive.org/download/microsoftSrcLeakCollection/ms_dos_3_30_oem_adaptation_kit_source_code.7z)"
+mv dos-3.30/SOURCE src
+rmdir dos-3.30
+git add -Af src/{PROGREF,SRC,AG.DOC,README.BF,README.DIS}
+commit '1987-07-24 05:00:02 +0000' 'MS-DOS 3.30 (part 1)'
+git add -Af src/UPDSRC
+commit '1988-02-02 06:00:02 +0000' 'MS-DOS 3.30 (part 2)'
+git rm -rq src
+
+# All files have dates in sequence alphabetically from 1999-10-31 16:33:52 +0000
+# to 1999-10-31 19:01:40 +0000. Use the earlier date.
+7z x "$(get_cached_path https://archive.org/download/microsoftSrcLeakCollection/ms_dos_6_0_source_code.7z)"
+mv dos-6.0 src
+git add -Af src
+commit '1999-10-31 19:01:40 +0000' 'MS-DOS 6.0'
 
 # The repo was force-pushed to reorganize and remove an insult. Discussed at
 # https://news.ycombinator.com/item?id=40163766. Strangely, both of these cooked
