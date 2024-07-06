d9ca3714d7e89b44d358ac126dbaf57f381d09af
Thalia Archibald <thalia@archibald.dev>
2024-07-05 19:48:59 -0700

    software-foundations: Fix QC title
    The name of the tool is actually QuickChick, not QuickCheck; it's not a
    typo. Coq QuickChick is a clone of Haskell QuickCheck.

--- scripts/software-foundations/get_versions.sh
+++ scripts/software-foundations/get_versions.sh
@@ -47,7 +47,7 @@ get_releases() {
     1.0 1.1 1.2 1.3 1.4 \
     1.5 1.5.1 1.5.2 1.5.3 1.5.4
 
-  get_releases 4 'QuickCheck: Property-Based Testing in Coq' qc \
+  get_releases 4 'QuickChick: Property-Based Testing in Coq' qc \
     1.0 \
     1.1 \
     1.2 1.2.1 \
