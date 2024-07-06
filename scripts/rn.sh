aae78bbd59aecc2a3aba0c43836bf10682b77336
Thalia Archibald <thalia@archibald.dev>
2024-07-06 08:23:41 -0700

    rn: Compare repost

--- scripts/rn.sh
+++ scripts/rn.sh
@@ -7,17 +7,23 @@ extract_post() {
   local post_id="$1"
   local file
   file="$(get_usenet_post "$post_id")"
-  sh <(jq -r '
+  chronic sh <(jq -r '
     def assert(pred; message): if pred | not then error(message) end;
     assert(length == 1; "expected 1 post") | .[0].body |
     assert(length == 1; "expected 1 body") | .[0].content
   ' "$file")
 }
 
+get_post_date() {
+  local post_id="$1"
+  jq -r '.[0].header.date' "$TOPLEVEL/cache/usenetarchives/$post_id.json"
+}
+
 mkdir rn
 cd rn
 git init -q
 
+# https://usenetarchives.com/threads.php?id=net.sources&y=NaN&r=NaN&p=78
 extract_post PDEzMzJAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 1 of 8)
 extract_post PDEzMzNAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 2 of 8)
 extract_post PDEzMzRAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 3 of 8)
@@ -27,11 +33,11 @@ extract_post PDEzMzdAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 
 extract_post PDEzMzhAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 7 of 8)
 extract_post PDEzMzlAc2RjcmRjZi5VVUNQPg # rn version 4.1 distribution kit (part 8 of 8)
 
-date="$(jq -r '.[0].header.date' "$TOPLEVEL/cache/usenetarchives/PDEzMzJAc2RjcmRjZi5VVUNQPg.json")"
 rm kit{1,2,3,4,5,6,7,8}isdone
 git add -A
 
-commit "Larry Wall <lwall@sdcrdcf.UUCP> $date" 'rn version 4.1
+commit "Larry Wall <lwall@sdcrdcf.UUCP> $(get_post_date PDEzMzJAc2RjcmRjZi5VVUNQPg)" \
+'rn version 4.1
 
 Source: rn version 4.1 distribution kit
 - part 1: https://usenetarchives.com/view.php?id=net.sources&mid=PDEzMzJAc2RjcmRjZi5VVUNQPg
@@ -42,3 +48,20 @@ Source: rn version 4.1 distribution kit
 - part 6: https://usenetarchives.com/view.php?id=net.sources&mid=PDEzMzdAc2RjcmRjZi5VVUNQPg
 - part 7: https://usenetarchives.com/view.php?id=net.sources&mid=PDEzMzhAc2RjcmRjZi5VVUNQPg
 - part 8: https://usenetarchives.com/view.php?id=net.sources&mid=PDEzMzlAc2RjcmRjZi5VVUNQPg'
+
+git rm -qr .
+
+# https://usenetarchives.com/threads.php?id=net.sources&y=NaN&r=NaN&p=80
+extract_post PDEzNThAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 1 of 8 (reposting 1,2,3 & 7)
+extract_post PDEzNTlAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 2 of 8 (reposting 1,2,3 & 7)
+extract_post PDEzNjBAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 3 of 8 (reposting 1,2,3 & 7)
+extract_post PDEzNjFAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 7 of 8 (reposting 1,2,3 & 7)
+# https://usenetarchives.com/threads.php?id=net.sources&y=NaN&r=NaN&p=82
+extract_post PDEzODBAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 4 of 8 (reposting 4,5,6 & 8)
+extract_post PDEzODFAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 5 of 8 (reposting 4,5,6 & 8)
+extract_post PDEzODJAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 8 of 8 (reposting 4,5,6 & 8)
+extract_post PDEzODNAc2RjcmRjZi5VVUNQPg # Rn version 4.1, part 6 of 8 (reposting 4,5,6 & 8)
+
+rm kit{1,2,3,4,5,6,7,8}isdone
+git add -A
+git diff --quiet --cached || echo 'Repost is not identical'
