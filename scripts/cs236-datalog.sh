cc245d631faefb115e6f96a70925ba68eac126f1
Thalia Archibald <thalia@archibald.dev>
2024-10-15 17:41:05 -0700

    cs236-datalog: Add versions saved from 2022

--- scripts/cs236-datalog.sh
+++ scripts/cs236-datalog.sh
@@ -3,14 +3,30 @@ set -eEuo pipefail
 
 . base.sh
 
+export TZ=America/Denver
+export AUTHOR='Cory Barker <barker@cs.byu.edu>'
+
 mkdir cs236-datalog
 cd cs236-datalog
 git init -q
 
-for file in project{1,2,3,4,5}-{tests,passoff}.zip; do
-  mkdir "${file%.zip}"
-  echo "Extracting $file"
-  unzip -q "$(get_cached_path "https://faculty.cs.byu.edu/~barker/cs236/tests/$file")" -d "${file%.zip}"
+for archive in "$TOPLEVEL"/files/cs236-datalog/2022/*.zip; do
+  dir="${archive%.zip}"
+  dir="${dir##*/}"
+  mkdir "$dir"
+  echo "Extracting ${archive#$TOPLEVEL/}"
+  unzip -q "$archive" -d "$dir"
 done
 git add -f .
-TZ=America/Denver commit 'Cory Barker <barker@cs.byu.edu> latest' 'Add project tests'
+commit latest 'Add project tests'
+
+git rm -qr --ignore-unmatch .
+for url in https://faculty.cs.byu.edu/~barker/cs236/tests/project{1,2,3,4,5}-{tests,passoff}.zip; do
+  dir="${url%.zip}"
+  dir="${dir##*/}"
+  mkdir "$dir"
+  echo "Extracting $url"
+  unzip -q "$(get_cached_path "$url")" -d "$dir"
+done
+git add -f .
+commit latest 'Update project tests'
