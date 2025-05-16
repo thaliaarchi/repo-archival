637d99d0405d9b0e077055803593eb846f63ebf5
Thalia Archibald <thalia@archibald.dev>
2025-05-16 00:01:30 -0700

    pik4ez-ascii: Fully automate

--- scripts/wspace/pik4ez-ascii.sh
+++ scripts/wspace/pik4ez-ascii.sh
@@ -9,53 +9,92 @@
 # https://gist.github.com/pik4ez/fb76073886531d9b888a5c86e870bccd shura-bura.ws
 # https://gist.github.com/pik4ez/e3d776091bae3a41e18ae1404bdccac1 lamarr.ws
 
-# TODO: Incorporate jonas.bf, automate further, and keep hedi_ascii.ws filename.
+# TODO: Incorporate jonas.bf.
 
-# brooks contains the entirety of the other repos at earlier dates,
-# however the history is messy:
+# brooks contains the entirety of the other .ws repos at earlier dates, however
+# the history is messy. jonas is separate.
 
-# 1. add gistfile1.txt
-# 2. edit gistfile1.txt (remove placeholder header)
-# 3. rename gistfile1.txt -> hedi_ascii.ws
-# 4. add brooks.ws
+# jonas:
+# 1. 2017-11-11 14:52:47 +0300: add jonas.bf
+
+# brooks:
+# 1. 2018-08-15 18:42:37 +0300
+#    add gistfile1.txt
+# 2. 2018-08-15 20:40:29 +0300
+#    edit gistfile1.txt (remove placeholder header)
+# 3. 2018-08-15 20:41:06 +0300
+#    rename gistfile1.txt -> hedi_ascii.ws
+# 4. 2018-08-16 20:02:46 +0300
+#    add brooks.ws
 #    add hopper.ws
 #    rename hedi_ascii.ws -> lamarr.ws
 #    add rameev.ws
 #    add shura-bura.ws
-# 5. edit brooks.ws (replace placeholder)
+# 5. 2018-08-16 20:05:05 +0300
+#    edit brooks.ws (replace placeholder)
 #    edit hopper.ws (replace placeholder)
 #    edit rameev.ws (replace placeholder)
 #    edit shura-bura.ws (replace placeholder)
-# 6. delete hopper.ws
+# 6. 2018-08-16 20:10:46 +0300
+#    delete hopper.ws
 #    delete lamarr.ws
 #    delete rameev.ws
 #    delete shura-bura.ws
 
+# hopper:
+# 1. 2018-08-16 20:06:38 +0300: add hopper.ws
+# 2. 2018-08-16 20:06:53 +0300: edit hopper.ws (replace placeholder)
+
+# rameev:
+# 1. 2018-08-16 20:07:43 +0300: add rameev.ws
+# 2. 2018-08-16 20:08:04 +0300: edit rameev.ws (replace placeholder)
+
+# shura-bura:
+# 1. 2018-08-16 20:08:53 +0300: add shura-bura.ws
+# 2. 2018-08-16 20:09:08 +0300: edit shura-bura.ws (replace placeholder)
+
+# lamarr:
+# 1. 2018-08-16 20:09:57 +0300: add lamarr.ws
+# 2. 2018-08-16 20:10:19 +0300: edit lamarr.ws (replace placeholder)
+
 clone_submodule https://gist.github.com/pik4ez/a2b0ece78c999e3b9ca54369ffd484f3 wspace/pik4ez-ascii
 cd wspace/pik4ez-ascii
 
-# Remove file deletions and fix lamarr.ws filename:
+# Fix the gistfile1.txt filename and drop file deletions.
+# Set the commit message to list the files added or modified (both are listed as
+# type M) and on successive occurrences of the same files changed, mark it with
+# "amend!".
+# Since filter-repo does not report renames, crudely check for the hedi_ascii.ws
+# -> lamarr.ws rename.
 git filter-repo --quiet \
-  --path-rename gistfile1.txt:lamarr.ws \
-  --path-rename hedi_ascii.ws:lamarr.ws \
+  --path-rename gistfile1.txt:hedi_ascii.ws \
   --commit-callback '
-    commit.file_changes = [c for c in commit.file_changes if c.type != b"D"]
-    commit.message = commit.author_date'
+    filenames = set(c.filename for c in commit.file_changes)
 
-# Squash placeholder text commits:
-#   pick 99e0669 1534347757 +0300
-#   squash 2e75f1c 1534354829 +0300
-#   pick 23b06bb 1534438966 +0300
-#   squash 138d65b 1534439105 +0300
-git rebase -i --committer-date-is-author-date --root
+    modified = [c.filename for c in commit.file_changes if c.type != b"D"
+      and not (c.filename == b"lamarr.ws" and b"hedi_ascii.ws" in filenames)];
+    if len(modified) > 1:
+      modified[-1] = b"and " + modified[-1]
+    commit.message = b"Add " + b", ".join(modified) + b"\n"
 
-# Set messages to names of files, since only changes are now adds:
+    global commits
+    if not "commits" in globals(): commits = set()
+    if commit.message in commits:
+      commit.message = b"squash! " + commit.message
+    else:
+      commits.add(commit.message)
+
+    commit.file_changes = [c for c in commit.file_changes if c.type != b"D"
+      or c.filename == b"hedi_ascii.ws" and b"lamarr.ws" in filenames]
+  '
+
+# Squash placeholder text fixup commits, making their author dates become the
+# committer dates of the base commits.
+GIT_EDITOR=: git rebase --root --autosquash --committer-date-is-author-date
+
+# Fix committer.
 git filter-repo --quiet \
   --commit-callback '
-    filenames = [c.filename for c in commit.file_changes]
-    if len(filenames) > 1:
-      filenames[-1] = b"and " + filenames[-1]
-    commit.message = b"Add " + b", ".join(filenames)
     commit.committer_name = commit.author_name
     commit.committer_email = commit.author_email
   '
