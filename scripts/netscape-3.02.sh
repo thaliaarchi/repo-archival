1230e2ed6fe4f4ee1a2c37f47fd9f06c2f2039c1
Thalia Archibald <thalia@archibald.dev>
2025-07-16 21:58:15 -0700

    netscape-3.02: Note Mocha engine

--- scripts/netscape-3.02.sh
+++ scripts/netscape-3.02.sh
@@ -12,13 +12,19 @@
 # was archived. It was uploaded to the Internet Archive 2011-10-28 22:20:21 UTC
 # by Jason Scott.
 #
-# Netscape 3.02 was released some time between August 1996 and July 1997 (https://en.wikipedia.org/wiki/Netscape_(web_browser)#Release_history).
+# Netscape 3.02 was released some time between 1996-08-19 and 1997-07-31 (https://en.wikipedia.org/wiki/Netscape_(web_browser)#Release_history).
 # For comparison, the first commit in the Gecko source tree was
 # 1998-03-28 02:44:41 +0000 (https://github.com/mozilla/gecko-dev/tree/3b56a9af51519d2e77e05efa672a13e6be2e9ebc).
 #
 # A commenter on IA notes that it only has the X Window front-end (xfe) and is
 # missing the Mac (macfe) and Windows (winfe) front-ends. A blog post remarks on
 # other missing components (https://virtuallyfun.com/2021/03/19/netscape-3-02-aka-hiding-in-plain-site/).
+
+# The Mocha engine for JavaScript 1.1 was extracted from this source and turned
+# into a standalone C library with native, WebAssembly, and JavaScript targets
+# (https://github.com/doodlewind/mocha1995). This version of JavaScript was
+# discussed in "JavaScript: The First 20 Years", Allen Wirfs-Brock and Brendan
+# Eich (2020) <https://wirfs-brock.com/allen/jshopl.pdf>.
 
 # https://archive.org/details/netscape-communicator-3-0-2-source
 tar xf "$(get_cached_path https://archive.org/download/netscape-communicator-3-0-2-source/ns302.tar.bz2)"
@@ -36,5 +42,3 @@ commit 'Jamie Zawinski <jwz@netscape.com> 2004-11-02 23:45:24 +0000,
 git rm -qr CVS dist
 find . -name .cvsignore -execdir git mv .cvsignore .gitignore \;
 commit 'Thalia Archibald <thalia@archibald.dev> 2023-11-15 17:55:33 -0800' 'Convert to git'
-
-git remote add origin https://github.com/thaliaarchi/netscape-3.02
